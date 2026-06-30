f0ef65d75d6eb6f6c4efaedec69405bdd831a526
furseal <mistery0408@gmail.com>
2026-06-30 17:46:19 +0900

    Update DBUtil.java

--- KRDProject-Hub/src/kr/util/DBUtil.java
+++ KRDProject-Hub/src/kr/util/DBUtil.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
 
 public class DBUtil {
 	private static final String DB_DRIVER = "oracle.jdbc.OracleDriver";
-	private static final String DB_URL = "jdbc:oracle:thin:@211.238.142.200:1521:xe";
-	private static final String DB_ID = "ateam01";
-	private static final String DB_PASSWORD = "a1234";
+	private static final String DB_URL = "jdbc:oracle:thin:@localhost:xe";
+	private static final String DB_ID = "";
+	private static final String DB_PASSWORD = "";
 
 	//Connection 객체를 생성해서 반환
 	public static Connection getConnection() throws ClassNotFoundException,SQLException {
